496d60cda73eef29cbac5fff14f81c47e342b138
Robbie <10761269+exrobbie@users.noreply.github.com>
2017-11-30 14:50:25 +0800

    changed name carousel to slider

--- src/simpleCarousel.js
+++ src/simpleCarousel.js
@@ -1,12 +1,12 @@
 ; (function ($) {
     $.fn.extend({
-        carousel: function (options) {
-            var $carousel = this;
+        slider: function (options) {
+            var $slider = this;
             $.each(options.imgList, function (index, item) {
-                $carousel.children('li').eq(index).css({"background": "url(" + item + ") center no-repeat", "backgroundSize":"100%"});
+                $slider.children('li').eq(index).css({"background": "url(" + item + ") center no-repeat"});
             });
 
-            $carousel.css({
+            $slider.css({
                 overflow: 'hidden',
                 position: 'relative',
                 width: '100%',
@@ -15,14 +15,15 @@
                 margin: '0'
             });
 
-            $carousel.children('li').css({
+            $slider.children('li').css({
                 position: 'absolute',
                 top: '0',
                 left: '0',
                 listStyle: 'none',
                 textAlign: 'center',
                 width: '100%',
-                height: '100%'
+                height: '100%',
+                backgroundSize: '100%'
             })
 
             var defaults = {
@@ -30,22 +31,22 @@
                 duration: 1200
             }
             var settings = $.extend(defaults, options);
-            var carouselWidth = $carousel.innerWidth(),
-                carouselHeight = $carousel.innerHeight(),
-                carouselLength = options.imgList.length,
+            var sliderWidth = $slider.innerWidth(),
+                sliderHeight = $slider.innerHeight(),
+                sliderLength = options.imgList.length,
                 currentShow = 0,
                 prevShow,
                 nextShow;
 
             function moveFoward() {
-                $carousel.children('li').eq(currentShow).animate({ 'left': '0px' }, settings.duration);
+                $slider.children('li').eq(currentShow).animate({ 'left': '0px' }, settings.duration);
                 if (currentShow == 0) {
-                    prevShow = carouselLength - 1;
+                    prevShow = sliderLength - 1;
                 } else {
                     prevShow = currentShow - 1;
                 }
 
-                if (currentShow == carouselLength - 1) {
+                if (currentShow == sliderLength - 1) {
                     nextShow = 0;
                     //set currentShow for next interval
                     currentShow = 0;
@@ -54,43 +55,43 @@
                     //set currentShow for next interval
                     currentShow++;
                 }
-                $carousel.children('li').eq(prevShow).animate({ 'left': -carouselWidth + 'px' }, settings.duration);
-                $carousel.children('li').eq(nextShow).css({ 'left': carouselWidth + 'px' });
+                $slider.children('li').eq(prevShow).animate({ 'left': -sliderWidth + 'px' }, settings.duration);
+                $slider.children('li').eq(nextShow).css({ 'left': sliderWidth + 'px' });
             }
 
             function moveBackward() {
-                $carousel.children('li').eq(currentShow).animate({ 'left': '0px' }, settings.duration);
+                $slider.children('li').eq(currentShow).animate({ 'left': '0px' }, settings.duration);
                 console.log('current', currentShow, 'previous', prevShow, 'next', nextShow)
-                if (currentShow == carouselLength - 1) {
+                if (currentShow == sliderLength - 1) {
                     prevShow = 0;
                 } else {
                     prevShow = currentShow + 1;
                 }
                 if (currentShow == 0) {
-                    nextShow == carouselLength - 1;
+                    nextShow == sliderLength - 1;
                     //set currentShow for next interval
-                    currentShow = carouselLength - 1;
+                    currentShow = sliderLength - 1;
                 } else {
                     nextShow = currentShow - 1;
                     //set currentShow for next interval
                     currentShow--;
                 }
-                $carousel.children('li').eq(prevShow).animate({ 'left': carouselWidth + 'px' }, settings.duration);
-                $carousel.children('li').eq(nextShow).css({ 'left': -carouselWidth + 'px' });
+                $slider.children('li').eq(prevShow).animate({ 'left': sliderWidth + 'px' }, settings.duration);
+                $slider.children('li').eq(nextShow).css({ 'left': -sliderWidth + 'px' });
             }
 
             function triggerFoward() {
-                clearInterval(carouselInterval);
+                clearInterval(sliderInterval);
                 moveFoward();
             }
 
             function triggerBackward() {
-                clearInterval(carouselInterval);
+                clearInterval(sliderInterval);
                 moveBackward();
             }
 
             moveFoward();
-            var carouselInterval = setInterval(moveFoward, settings.interval);
+            var sliderInterval = setInterval(moveFoward, settings.interval);
 
             return this;
         }
